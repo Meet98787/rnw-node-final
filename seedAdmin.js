b5e7000441b69c8e2932194120d22e69a952b8bd
meet98787 <meet98787@gmail.com>
2024-05-20 19:16:46 +0530

    final

---
+++ seedAdmin.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const User = require('./models/User');
+require('dotenv').config();
+
+const seedAdmin = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  // const hashedPassword = await bcrypt.hash('123123', salt);
+
+  const admin = new User({
+    username: 'admin',
+    email: 'admin@gmail.com',
+    password: '123123',
+    role: 'admin'
+  });
+
+  await admin.save();
+  console.log('Admin user created');
+  mongoose.disconnect();
+};
+
+seedAdmin();
